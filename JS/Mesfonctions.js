4c80001c103c07138304706d2cb01c45e235b5b9
Nore15 <nm.akhem@gmail.com>
2022-07-30 16:41:41 +0200

    git

--- JS/Mesfonctions.js
+++ JS/Mesfonctions.js
@@ -2,15 +2,33 @@ function Exo1()
 {
         console.log('carré cliqué')
         $('.vert-center').hide()
-        let offsetTop = Math.floor(Math.random() * 300);
-        let offsetLeft = Math.floor(Math.random() * 300);
+        let screenWidth = screen.width;
+        let screenHeight = screen.height;
+        let score = $('#score').text()
+        console.log(score)
+        $('#score').html(parseInt(score) + 1)
+        let offsetTop = Math.floor(Math.random() * (screenHeight -100));
+        let offsetLeft = Math.floor(Math.random() * (screenWidth -100));
         console.log(offsetTop,$('.vert-center').offset().top);
-        console.log(offsetLeft,$('.vert-center').offset().top);
-        $('.vert-center').offset({top: offsetTop, left: offsetLeft})
+        console.log(offsetLeft,$('.vert-center').offset().left);
+        $('.vert-center').css({top: offsetTop, left: offsetLeft})
         $('.vert-center').show()
         // récupérer les largeurs et la hauteur de l'écran
-        var screenWidth = screen.width;
-        var screenHeight = screen.height;
-        var x = document.getElementById("screen");
-        x.innerHTML = "Device Screen: width: " + screenWidth + ", height: " + screenHeight + ".";
+        // var x = document.getElementById("screen");
+        // x.innerHTML = "Device Screen: width: " + screenWidth + ", height: " + screenHeight + ".";
+}
+
+function decrease()
+{
+        let time = $('#time').text()
+        console.log(time)
+        if (parseInt(time) > 0){
+                $('#time').html(parseInt(time) - 1)
+        }
+}
+
+var intervalId = null;
+function finish() {
+  clearInterval(intervalId);
+  document.getElementById("time").innerHTML = "TERMINE!";
 }
